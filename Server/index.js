bedd0d5c0b9ecd9fb6f47c6ed5b7f2cb483d4c5f
Govind B Chandran <govindchandran150@gmail.com>
2022-05-15 17:34:47 +0530

    Add files via upload

--- Server/index.js
+++ Server/index.js
@@ -13,5 +13,3 @@ const CONNECTION_URL = "mongodb+srv://govindchandran:govindchandran123@cluster0.
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true}).then(() => app.listen(PORT, () => console.log("Server running on 5000"))).catch((error) => console.error(error.message));
-
-mongoose.set('useFindAndModify', false);
